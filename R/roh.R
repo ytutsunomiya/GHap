b4a05f36ede4ce402672c6407857b6655dc62ac3
andrevn16 <83076245+andrevn16@users.noreply.github.com>
2021-04-26 18:02:00 -0300

    Included parallelization under Windows

--- R/roh.R
+++ R/roh.R
@@ -1,6 +1,6 @@
 #Function: ghap.roh
 #License: GPLv3 or later
-#Modification date: 07 Feb 2021
+#Modification date: 26 Apr 2021
 #Written by: Yuri Tani Utsunomiya
 #Contact: ytutsunomiya@gmail.com
 #Description: Map streches of homozygous genotypes
@@ -31,16 +31,6 @@ ghap.roh <- function(
   if(only.active.samples == FALSE){
     phase$id.in <- rep(TRUE,times=2*phase$nsamples)
     phase$nsamples.in <- length(which(phase$id.in))/2
-  }
-  
-  # Get number of cores-------------------------------------------------------------------------------
-  if(Sys.info()["sysname"] == "Windows"){
-    if(ncores > 1 & verbose == TRUE){
-      cat("\nParallelization not supported yet under Windows (using a single core).")
-    }
-    ncores <- 1
-  }else{
-    ncores <- min(c(detectCores(), ncores))
   }
   
   # Initialize lookup table----------------------------------------------------------------------------
@@ -171,6 +161,7 @@ ghap.roh <- function(
 
   
   # Find runs of homozygosity--------------------------------------------------------------------------
+  ncores <- min(c(detectCores(), ncores))
   ids <- unique(phase$id[which(phase$id.in)])
   outruns <- NULL
   if(method == "hmm" & is.null(freq) == TRUE){
@@ -199,7 +190,9 @@ ghap.roh <- function(
     geno <- ghap.pslice(phase = phase, ids = ids, ncores = ncores,
                         markers = mkrs, unphase = TRUE, lookup = lookup)
     if(Sys.info()["sysname"] == "Windows"){
-      segs <- lapply(FUN = rohfun, X = 1:length(ids))
+      cl <- makeCluster(ncores)
+      segs <- unlist(parLapply(cl = cl, fun = rohfun, X = 1:length(ids)))
+      stopCluster(cl)
     }else{
       segs <- mclapply(FUN = rohfun, X = 1:length(ids), mc.cores = ncores)
     }
