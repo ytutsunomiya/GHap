9650c6b668c3680e8ccc16df3125432346f1b773
ytutsunomiya <ytutsunomiya@gmail.com>
2021-05-13 16:59:19 -0300

    generalized for multiple examples

--- R/makefile.R
+++ R/makefile.R
@@ -1,16 +1,42 @@
 #Function: ghap.makefile
 #License: GPLv3 or later
-#Modification date: 28 Apr 2021
+#Modification date: 13 May 2021
 #Written by: Yuri Tani Utsunomiya & Marco Milanesi
 #Contact: ytutsunomiya@gmail.com, marco.milanesi.mm@gmail.com
-#Description: Create a copy of the example file in a temporary directory
+#Description: Create a copy of an example file in a temporary directory
 
-ghap.makefile <- function(verbose = TRUE){
+ghap.makefile <- function(
+  dataset = "human_hapmap3_chr2",
+  format = "phase",
+  verbose = TRUE
+){
   
-  #Download files to temporary directory
+  # Check if format is supported -----------------------------------------------
+  extension <- NULL
+  extension$phase <- c(".markers",".samples",".phaseb")
+  extension$plink <- c(".bed",".bim",".fam")
+  extension$haplo <- c(".hapgenotypesb",".hapsamples",".hapalleles")
+  if(format %in% c("phase","plink","haplo") == FALSE){
+    emsg <- paste("\nFormat", format, "is not supported")
+    stop(emsg)
+  }
+  
+  # Check if requested dataset exists ------------------------------------------
+  # Code borrowed from stackoverflow
+  urlfile <- paste0("https://github.com/ytutsunomiya/GHap/blob/main/datasets/",
+                  dataset, "_", format, ".zip?raw=true")
+  con <- url(urlfile)
+  check <- suppressWarnings(try(open.connection(con,open="rt",timeout=2),silent=T)[1])
+  suppressWarnings(try(close.connection(con),silent=T))
+  check <- ifelse(is.null(check),TRUE,FALSE)
+  if(check == FALSE){
+    emsg <- paste("\nCould not find dataset", dataset, "with format", format)
+    stop(emsg)
+  }
+  
+  # Download files to temporary directory ---------------------------------------
   tmpdir <- tempdir()
-  tmpfile <- tempfile("HapMap3_chr2", fileext = ".zip")
-  urlfile <- "https://github.com/ytutsunomiya/GHap/blob/main/HapMap3_chr2.zip?raw=true"
+  tmpfile <- tempfile(dataset, fileext = ".zip")
   if(Sys.info()["sysname"] == "Windows"){
     cpfile <- download.file(url = urlfile, destfile = tmpfile, quiet = TRUE, mode = "wb")
   }else{
@@ -19,8 +45,8 @@ ghap.makefile <- function(verbose = TRUE){
   unzip(zipfile = tmpfile, exdir = tmpdir)
   rfile <- file.remove(tmpfile)
   
-  #Check whether files have been copied and extracted
-  outfiles <- paste(tmpdir,"/human",c(".markers",".samples",".phase"),sep="")
+  # Check whether files have been copied and extracted ---------------------------
+  outfiles <- paste0(tmpdir, "/", dataset, unlist(extension[format]))
   exfile <- sum(file.exists(outfiles))
   if(cpfile == 0 & exfile == 3 & verbose == TRUE){
     cat("\nFiles successfully created!\n")
@@ -32,7 +58,7 @@ ghap.makefile <- function(verbose = TRUE){
     stop("Error when copying the files!\nThis function needs super-user privilege and a working untar engine.")
   }
   
-  #Return file names
+  # Return file names ------------------------------------------------------------
   return(outfiles)
   
 }
