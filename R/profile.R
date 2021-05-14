c4e87cedb05893e5cea0f9f5b3780bcc424776c6
ytutsunomiya <ytutsunomiya@gmail.com>
2021-05-14 16:55:51 -0300

    Generalized for phase, haplo and plink

--- R/profile.R
+++ R/profile.R
@@ -1,13 +1,12 @@
 #Function: ghap.profile
 #License: GPLv3 or later
-#Modification date: 12 May 2021
+#Modification date: 14 May 2021
 #Written by: Yuri Tani Utsunomiya
 #Contact: ytutsunomiya@gmail.com
 #Description: Compute individual profiles based on HapAllele or marker scores
 
 ghap.profile <- function(
-  phase = NULL,
-  haplo = NULL,
+  object,
   score,
   only.active.samples=TRUE,
   batchsize=NULL,
@@ -15,81 +14,83 @@ ghap.profile <- function(
   verbose=TRUE
 ){
   
-  #Check if phase or haplo was provided
-  if(is.null(phase) & is.null(haplo)){
-    stop("A GHap.phase or GHap.haplo must be provided.")
+  # Check if object is a GHap.phase object -------------------------------------
+  obtypes <- c("GHap.phase","GHap.haplo","GHap.plink")
+  if(class(object) %in% obtypes == FALSE){
+    stop("\nInput data must be a valid GHap object (phase, haplo or plink).")
   }
+  fac <- c(2,1,1)
+  names(fac) <- obtype
   
-  #Check if both phase and haplo were provided
-  if(is.null(phase) == FALSE & is.null(haplo) == FALSE){
-    stop("Provide only one GHap.phase or GHap.haplo object.")
+  # Initialize lookup table ----------------------------------------------------
+  lookup <- rep(NA,times=256)
+  lookup[1:2] <- c(0,1)
+  d <- 10
+  i <- 3
+  while(i <= 256){
+    b <- d + lookup[1:(i-1)]
+    lookup[i:(length(b)+i-1)] <- b
+    i <- i + length(b)
+    d <- d*10
   }
+  lookup <- sprintf(fmt="%08d", lookup)
+  if(class(object) != "GHap.phase"){
+    lookup <- sapply(lookup, function(i){intToUtf8(rev(utf8ToInt(i)))})
+  }
+  ncores <- min(c(detectCores(), ncores))
   
-  #Scoring for haplo
-  if(is.null(haplo) == FALSE){
-    
-    #Check if haplo is a GHap.haplo object
-    if(class(haplo) != "GHap.haplo"){
-      stop("Argument haplo must be a GHap.haplo object.")
-    }
+  # Scoring for haplo ----------------------------------------------------------
+  if(class(object) == "GHap.haplo"){
     
     #Check if inactive alleles and samples should be reactived
     if(only.active.samples == FALSE){
-      haplo$id.in <- rep(TRUE,times=haplo$nsamples)
-      haplo$nsamples.in<-length(which(haplo$id.in))
+      object$id.in <- rep(TRUE,times=object$nsamples)
+      object$nsamples.in<-length(which(object$id.in))
     }
     
     #Prepare score vectors
-    score$HAPLOtmp <- paste(score$BLOCK,score$CHR,score$BP1,score$BP2,score$ALLELE,sep="_")
+    score$HAPLOtmp <- paste(score$BLOCK,score$CHR,
+                            score$BP1,score$BP2,score$ALLELE,sep="_")
     haps <- NULL
-    haps$HAPLOtmp <- paste(haplo$block,haplo$chr,haplo$bp1,haplo$bp2,haplo$allele,sep="_")
+    haps$HAPLOtmp <- paste(object$block,object$chr,
+                           object$bp1,object$bp2,object$allele,sep="_")
     haps$IDX <- 1:length(haps$HAPLOtmp)
     haps <- data.frame(haps,stringsAsFactors = FALSE)
-    haps <- merge(x = haps, y = score, by.x = "HAPLOtmp", by.y = "HAPLOtmp", all.x=TRUE,sort=FALSE)
+    haps <- merge(x = haps, y = score, by.x = "HAPLOtmp", by.y = "HAPLOtmp",
+                  all.x=TRUE,sort=FALSE)
     haps <- haps[is.na(haps$SCORE) == FALSE,]
     
     #Log message
     if(nrow(haps) != nrow(score)){
-      stop("From ",nrow(score)," HapAlleles declared but ",nrow(haps)," were found.\n")
+      emsg <- paste0("From ",nrow(score)," HapAlleles declared but ",
+                     nrow(haps)," were found.\n")
+      stop(emsg)
     }
-    
-    # Generate lookup table
-    lookup <- rep(NA,times=256)
-    lookup[1:2] <- c(0,1)
-    d <- 10
-    i <- 3
-    while(i <= 256){
-      b <- d + lookup[1:(i-1)]
-      lookup[i:(length(b)+i-1)] <- b
-      i <- i + length(b)
-      d <- d*10
-    }
-    lookup <- sprintf(fmt="%08d", lookup)
-    lookup <- sapply(lookup, function(i){intToUtf8(rev(utf8ToInt(i)))})
     
     #Generate batch index
     if(is.null(batchsize) == TRUE){
-      batchsize <- ceiling(haplo$nalleles.in/10)
+      batchsize <- ceiling(object$nalleles.in/10)
     }
-    if(batchsize > haplo$nalleles.in){
-      batchsize <- haplo$nalleles.in
+    if(batchsize > object$nalleles.in){
+      batchsize <- object$nalleles.in
     }
-    activealleles <- which(haplo$allele.in)
-    nbatches <- round(haplo$nalleles.in/(batchsize),digits=0) + 1
+    activealleles <- which(object$allele.in)
+    nbatches <- round(object$nalleles.in/(batchsize),digits=0) + 1
     mybatch <- paste("B",1:nbatches,sep="")
     batch <- rep(mybatch,each=batchsize)
-    batch <- batch[1:haplo$nalleles.in]
+    batch <- batch[1:object$nalleles.in]
     mybatch <- unique(batch)
     nbatches <- length(mybatch)
     
     #Log message
     if(verbose == TRUE){
-      cat("Processing ", haplo$nalleles.in, " HapAlleles in ", nbatches, " batches.\n", sep="")
+      cat("Processing ", object$nalleles.in, " HapAlleles in ",
+          nbatches, " batches.\n", sep="")
       cat("Inactive alleles will be ignored.\n")
     }
     
     #auxiliary function
-    score.FUN <- function(j){
+    haplo_score.FUN <- function(j){
       x <- hap.geno[j,]
       xname <- as.numeric(rownames(hap.geno)[j])
       row <- which(haps$IDX == xname)
@@ -98,26 +99,29 @@ ghap.profile <- function(
     }
     
     #Iterate batches
-    ncores <- min(c(detectCores(), ncores))
     out <- NULL
-    out$POP <- haplo$pop[haplo$id.in]
-    out$ID <- haplo$id[haplo$id.in]
-    out$SCORE <- rep(0, times = haplo$nsamples.in)
+    out$POP <- object$pop[object$id.in]
+    out$ID <- object$id[object$id.in]
+    out$SCORE <- rep(0, times = object$nsamples.in)
     sumalleles <- 0
     for(i in 1:nbatches){
       idx <- which(batch == mybatch[i])
       slice <- activealleles[idx]
-      hap.geno <- ghap.slice(object = haplo, ids = which(haplo$id.in), variants = slice,
-                             index = TRUE, lookup = lookup, ncores = ncores)
+      hap.geno <- ghap.slice(object = haplo, ids = which(object$id.in),
+                             variants = slice, index = TRUE,
+                             lookup = lookup, ncores = ncores)
       if(Sys.info()["sysname"] == "Windows"){
         cl <- makeCluster(ncores)
-        a <- unlist(parLapply(cl = cl, fun = score.FUN, X = 1:nrow(hap.geno)))
+        clusterEvalQ(cl, library(Matrix))
+        varlist <- list("hap.geno","haps")
+        clusterExport(cl = cl, varlist = varlist, envir=environment())
+        a <- unlist(parLapply(cl = cl, fun = haplo_score.FUN, X = 1:nrow(hap.geno)))
         stopCluster(cl)
         a <- data.frame(matrix(a, nrow=nrow(hap.geno), byrow=TRUE))
         a <- colSums(a)
         out$SCORE <- out$SCORE + a
       }else{
-        a <- unlist(mclapply(X = 1:nrow(hap.geno), FUN = score.FUN, mc.cores = ncores))
+        a <- unlist(mclapply(X = 1:nrow(hap.geno), FUN = haplo_score.FUN, mc.cores = ncores))
         a <- data.frame(matrix(a, nrow=nrow(hap.geno), byrow=TRUE))
         a <- colSums(a)
         out$SCORE <- out$SCORE + a
@@ -131,25 +135,20 @@ ghap.profile <- function(
     
   }
   
-  #Scoring for phase
-  if(is.null(phase) == FALSE){
-    
-    #Check if phase is a GHap.phase object
-    if(is.null(phase) == FALSE & class(phase) != "GHap.phase"){
-      stop("Argument phase must be a GHap.phase object.")
-    }
+  # Scoring for phase ----------------------------------------------------------
+  if(class(phase) %in% c("GHap.phase","GHap.plink")){
     
     #Check if inactive samples should be reactived
-    if(only.active.samples == FALSE){
-      phase$id.in <- rep(TRUE,times=2*phase$nsamples)
-      phase$nsamples.in <- length(which(phase$id.in))/2
+      if(only.active.samples == FALSE){
+      object$id.in <- rep(TRUE,times=fac[class(object)]*object$nsamples)
+      object$nsamples.in <- length(which(object$id.in))/fac[class(object)]
     }
     
     #Check if all markers exist
-    idx <- which(score$MARKER %in% phase$marker)
+    idx <- which(score$MARKER %in% object$marker)
     if(length(idx) != nrow(score)){
       emsg <- paste("\nFrom", nrow(score), "markers declared in the score data frame only",
-                    length(idx), "were present in the GHap.phase object.")
+                    length(idx), "were present in the GHap object.")
       stop(emsg)
     }
     
@@ -162,8 +161,8 @@ ghap.profile <- function(
     }
     
     #Check if alleles exist
-    midx <- which(phase$marker %in% score$MARKER)
-    markers <- data.frame(MARKER = phase$marker[midx], A0 = phase$A0[midx], A1 = phase$A1[midx],
+    midx <- which(object$marker %in% score$MARKER)
+    markers <- data.frame(MARKER = object$marker[midx], A0 = object$A0[midx], A1 = object$A1[midx],
                           stringsAsFactors = FALSE)
     score <- merge(x = score, y = markers, by = "MARKER", sort=FALSE)
     score$isA0 <- as.integer(score$A0 == score$ALLELE)
@@ -175,20 +174,6 @@ ghap.profile <- function(
                     length(idx), "had alien alleles.")
       stop(emsg)
     }
-    
-    # Initialize lookup table
-    lookup <- rep(NA,times=256)
-    lookup[1:2] <- c(0,1)
-    d <- 10
-    i <- 3
-    while(i <= 256){
-      b <- d + lookup[1:(i-1)]
-      lookup[i:(length(b)+i-1)] <- b
-      i <- i + length(b)
-      d <- d*10
-    }
-    lookup <- sprintf(fmt="%08d", lookup)
-    ncores <- min(c(detectCores(), ncores))
     
     #Generate batch index
     nmarkers <- nrow(score)
@@ -215,9 +200,9 @@ ghap.profile <- function(
     }
     
     #auxiliary function
-    score.FUN <- function(j, phase.geno, score){
-      x <- phase.geno[j,]
-      xname <- rownames(phase.geno)[j]
+    marker_score.FUN <- function(j){
+      x <- marker.geno[j,]
+      xname <- rownames(marker.geno)[j]
       row <- which(score$MARKER == xname)
       if(score$isA0[row] == 1){
         tmp <- x
@@ -230,27 +215,28 @@ ghap.profile <- function(
     }
     
     #Iterate batches
-    out <- unique(cbind(phase$pop[phase$id.in], phase$id[phase$id.in]))
+    out <- unique(cbind(object$pop[object$id.in], object$id[object$id.in]))
     out <- data.frame(out, stringsAsFactors = FALSE)
     colnames(out) <- c("POP","ID")
-    out$SCORE <- rep(0, times = phase$nsamples.in)
+    out$SCORE <- rep(0, times = object$nsamples.in)
     summarkers <- 0
     for(i in 1:nbatches){
       idx <- id1[i]:id2[i]
-      phase.geno <- ghap.slice(object = phase, ids = out$ID, variants = score$MARKER[idx],
+      marker.geno <- ghap.slice(object = phase, ids = out$ID, variants = score$MARKER[idx],
                                lookup = lookup, ncores = ncores, unphase = TRUE)
       if(Sys.info()["sysname"] == "Windows"){
         cl <- makeCluster(ncores)
-        a <- unlist(parLapply(cl = cl, fun = score.FUN, X = 1:nrow(phase.geno),
-                              phase.geno = phase.geno, score = score[idx,]))
+        clusterEvalQ(cl, library(Matrix))
+        varlist <- list("marker.geno","score")
+        clusterExport(cl = cl, varlist = varlist, envir=environment())
+        a <- unlist(parLapply(cl = cl, fun = marker_score.FUN, X = 1:nrow(marker.geno))
         stopCluster(cl)
-        a <- data.frame(matrix(a, nrow=nrow(phase.geno), byrow=TRUE))
+        a <- data.frame(matrix(a, nrow=nrow(marker.geno), byrow=TRUE))
         a <- colSums(a)
         out$SCORE <- out$SCORE + a
       }else{
-        a <- unlist(mclapply(X = 1:nrow(phase.geno), FUN = score.FUN, mc.cores = ncores,
-                             phase.geno = phase.geno, score = score[idx,]))
-        a <- data.frame(matrix(a, nrow=nrow(phase.geno), byrow=TRUE))
+        a <- unlist(mclapply(X = 1:nrow(marker.geno), FUN = marker_score.FUN, mc.cores = ncores))
+        a <- data.frame(matrix(a, nrow=nrow(marker.geno), byrow=TRUE))
         a <- colSums(a)
         out$SCORE <- out$SCORE + a
       }
