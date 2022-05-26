b101902319062a42068f4fd6538260c02bfcbcac
ytutsunomiya <ytutsunomiya@gmail.com>
2022-05-26 13:29:14 -0300

    Update profile.R

--- R/profile.R
+++ R/profile.R
@@ -1,6 +1,6 @@
 #Function: ghap.profile
 #License: GPLv3 or later
-#Modification date: 05 Oct 2021
+#Modification date: 26 May 2022
 #Written by: Yuri Tani Utsunomiya
 #Contact: ytutsunomiya@gmail.com
 #Description: Compute individual profiles based on HapAllele or marker scores
@@ -22,101 +22,6 @@ ghap.profile <- function(
   fac <- c(2,1,1)
   names(fac) <- obtypes
   ncores <- min(c(detectCores(), ncores))
-  
-  # Scoring for haplo ----------------------------------------------------------
-  if(class(object) == "GHap.haplo"){
-    
-    #Check if inactive alleles and samples should be reactived
-    if(only.active.samples == FALSE){
-      object$id.in <- rep(TRUE,times=object$nsamples)
-      object$nsamples.in<-length(which(object$id.in))
-    }
-    
-    #Prepare score vectors
-    score$HAPLOtmp <- paste(score$BLOCK,score$CHR,
-                            score$BP1,score$BP2,score$ALLELE,sep="_")
-    haps <- NULL
-    haps$HAPLOtmp <- paste(object$block,object$chr,
-                           object$bp1,object$bp2,object$allele,sep="_")
-    haps$IDX <- 1:length(haps$HAPLOtmp)
-    haps <- data.frame(haps,stringsAsFactors = FALSE)
-    haps <- merge(x = haps, y = score, by.x = "HAPLOtmp", by.y = "HAPLOtmp",
-                  all.x=TRUE,sort=FALSE)
-    haps <- haps[is.na(haps$SCORE) == FALSE,]
-    
-    #Log message
-    if(nrow(haps) != nrow(score)){
-      emsg <- paste0("From ",nrow(score)," HapAlleles declared but ",
-                     nrow(haps)," were found.\n")
-      stop(emsg)
-    }
-    
-    #Generate batch index
-    if(is.null(batchsize) == TRUE){
-      batchsize <- ceiling(object$nalleles.in/10)
-    }
-    if(batchsize > object$nalleles.in){
-      batchsize <- object$nalleles.in
-    }
-    activealleles <- which(object$allele.in)
-    nbatches <- round(object$nalleles.in/(batchsize),digits=0) + 1
-    mybatch <- paste("B",1:nbatches,sep="")
-    batch <- rep(mybatch,each=batchsize)
-    batch <- batch[1:object$nalleles.in]
-    mybatch <- unique(batch)
-    nbatches <- length(mybatch)
-    
-    #Log message
-    if(verbose == TRUE){
-      cat("Processing ", object$nalleles.in, " HapAlleles in ",
-          nbatches, " batches.\n", sep="")
-      cat("Inactive alleles will be ignored.\n")
-    }
-    
-    #auxiliary function
-    haplo_score.FUN <- function(j){
-      x <- hap.geno[j,]
-      xname <- as.numeric(rownames(hap.geno)[j])
-      row <- which(haps$IDX == xname)
-      x <- (x - haps$CENTER[row])/haps$SCALE[row]
-      return(x*haps$SCORE[row])
-    }
-    
-    #Iterate batches
-    out <- NULL
-    out$POP <- object$pop[object$id.in]
-    out$ID <- object$id[object$id.in]
-    out$SCORE <- rep(0, times = object$nsamples.in)
-    sumalleles <- 0
-    for(i in 1:nbatches){
-      idx <- which(batch == mybatch[i])
-      slice <- activealleles[idx]
-      hap.geno <- ghap.slice(object = object, ids = which(object$id.in),
-                             variants = slice, index = TRUE, ncores = ncores)
-      if(Sys.info()["sysname"] == "Windows"){
-        cl <- makeCluster(ncores)
-        clusterEvalQ(cl, library(Matrix))
-        varlist <- list("hap.geno","haps")
-        clusterExport(cl = cl, varlist = varlist, envir=environment())
-        a <- unlist(parLapply(cl = cl, fun = haplo_score.FUN, X = 1:nrow(hap.geno)))
-        stopCluster(cl)
-        a <- data.frame(matrix(a, nrow=nrow(hap.geno), byrow=TRUE))
-        a <- colSums(a)
-        out$SCORE <- out$SCORE + a
-      }else{
-        a <- unlist(mclapply(X = 1:nrow(hap.geno), FUN = haplo_score.FUN, mc.cores = ncores))
-        a <- data.frame(matrix(a, nrow=nrow(hap.geno), byrow=TRUE))
-        a <- colSums(a)
-        out$SCORE <- out$SCORE + a
-      }
-      if(verbose == TRUE){
-        sumalleles <- sumalleles + length(idx)
-        cat(sumalleles, "HapAlleles processed.\r")
-      }
-    }
-    out <- data.frame(out,stringsAsFactors = FALSE)
-    
-  }
   
   # Scoring for phase ----------------------------------------------------------
   if(class(object) %in% c("GHap.phase","GHap.plink")){
@@ -157,6 +62,10 @@ ghap.profile <- function(
                     length(idx), "had alien alleles.")
       stop(emsg)
     }
+    markers <- 1:phase$nmarkers
+    names(markers) <- phase$marker
+    score$IDX <- markers[score$MARKER]
+    score <- score[order(score$IDX),]
     
     #Generate batch index
     nmarkers <- nrow(score)
@@ -183,18 +92,11 @@ ghap.profile <- function(
     }
     
     #auxiliary function
-    marker_score.FUN <- function(j){
-      x <- marker.geno[j,]
-      xname <- rownames(marker.geno)[j]
-      row <- which(score$MARKER == xname)
-      if(score$isA0[row] == 1){
-        tmp <- x
-        tmp[which(x == 0)] <- 2
-        tmp[which(x == 2)] <- 0
-        x <- tmp
-      }
-      x <- (x - score$CENTER[row])/score$SCALE[row]
-      return(x*score$SCORE[row])
+    flip.FUN <- function(x){
+      tmp <- x
+      tmp[which(x == 0)] <- 2
+      tmp[which(x == 2)] <- 0
+      return(tmp)
     }
     
     #Iterate batches
@@ -205,26 +107,26 @@ ghap.profile <- function(
     summarkers <- 0
     for(i in 1:nbatches){
       idx <- id1[i]:id2[i]
-      marker.geno <- ghap.slice(object = object, ids = out$ID, variants = score$MARKER[idx],
-                                impute = TRUE, ncores = ncores, unphase = TRUE)
-      if(Sys.info()["sysname"] == "Windows"){
-        cl <- makeCluster(ncores)
-        clusterEvalQ(cl, library(Matrix))
-        varlist <- list("marker.geno","score")
-        clusterExport(cl = cl, varlist = varlist, envir=environment())
-        a <- unlist(parLapply(cl = cl, fun = marker_score.FUN, X = 1:nrow(marker.geno)))
-        stopCluster(cl)
-        a <- data.frame(matrix(a, nrow=nrow(marker.geno), byrow=TRUE))
-        a <- colSums(a)
-        out$SCORE <- out$SCORE + a
-      }else{
-        a <- unlist(mclapply(X = 1:nrow(marker.geno), FUN = marker_score.FUN, mc.cores = ncores))
-        a <- data.frame(matrix(a, nrow=nrow(marker.geno), byrow=TRUE))
-        a <- colSums(a)
-        out$SCORE <- out$SCORE + a
+      scoretmpA0 <- score[idx,]
+      scoretmpA1 <- scoretmpA0[which(scoretmpA0$isA1 == 1),]
+      scoretmpA0 <- scoretmpA0[which(scoretmpA0$isA0 == 1),]
+      if(nrow(scoretmpA0) > 0){
+        Ztmp <- ghap.slice(object = object, ids = out$ID,
+                           variants = scoretmpA0$MARKER, transposed = FALSE,
+                           impute = TRUE, ncores = ncores, unphase = TRUE)
+        Ztmp <- apply(X = Ztmp, MARGIN = 2, FUN = flip.FUN)
+        Ztmp <- (Ztmp - scoretmpA0$CENTER)/scoretmpA0$SCALE
+        out$SCORE <- out$SCORE + as.numeric(scoretmpA0$SCORE%*%Ztmp)
+      }
+      if(nrow(scoretmpA1) > 0){
+        Ztmp <- ghap.slice(object = object, ids = out$ID,
+                           variants = scoretmpA1$MARKER, transposed = FALSE,
+                           impute = TRUE, ncores = ncores, unphase = TRUE)
+        Ztmp <- (Ztmp - scoretmpA1$CENTER)/scoretmpA1$SCALE
+        out$SCORE <- out$SCORE + as.numeric(scoretmpA1$SCORE%*%Ztmp)
       }
       if(verbose == TRUE){
-        summarkers <- summarkers + length(idx)
+        summarkers <- summarkers + nrow(scoretmpA0) + nrow(scoretmpA1)
         cat(summarkers, "markers processed.\r")
       }
     }
