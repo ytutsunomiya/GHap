b99e60473f05fb45c032a3f7b1558a0dd6a8e35e
ytutsunomiya <ytutsunomiya@gmail.com>
2022-06-02 13:20:41 -0300

    Update hap2plink.R

--- R/hap2plink.R
+++ R/hap2plink.R
@@ -1,17 +1,17 @@
 #Function: ghap.hap2plink
 #License: GPLv3 or later
-#Modification date: 11 Sep 2020
+#Modification date: 2 Jun 2022
 #Written by: Yuri Tani Utsunomiya & Marco Milanesi
 #Contact: ytutsunomiya@gmail.com, marco.milanesi.mm@gmail.com
 #Description: Convert haplotype allele counts to plink bed/bim/fam
 
 ghap.hap2plink <- function(
-  haplo,
+  object,
   outfile
 ){
   
   #Check if haplo is a GHap.haplo object
-  if(class(haplo) != "GHap.haplo"){
+  if(class(object) != "GHap.haplo"){
     stop("Argument haplo must be a GHap.haplo object.")
   }
   
@@ -30,15 +30,15 @@ ghap.hap2plink <- function(
   }
   
   #Generate fam file
-  famfile <- cbind(haplo$pop,haplo$id,"0 0 0 -9")
+  famfile <- cbind(object$pop,object$id,"0 0 0 -9")
   fwrite(x = as.data.table(famfile), file = fam,
          quote = FALSE, sep=" ", row.names = FALSE, col.names = FALSE)
   
   #Generate bim file
-  bimfile <- data.frame(CHR = haplo$chr,
-                        SNP = paste(haplo$block, haplo$bp1, haplo$bp2, haplo$allele, sep="_"),
-                        CM = (haplo$bp1+haplo$bp2)/2e+6,
-                        BP = (haplo$bp1+haplo$bp2)/2,
+  bimfile <- data.frame(CHR = object$chr,
+                        SNP = paste(object$block, object$bp1, object$bp2, object$allele, sep="_"),
+                        CM = (object$bp1+object$bp2)/2e+6,
+                        BP = (object$bp1+object$bp2)/2,
                         A1 = "N",
                         A2 = "H")
   bimfile$BP <- as.integer(bimfile$BP)
@@ -46,6 +46,6 @@ ghap.hap2plink <- function(
          row.names = FALSE, col.names = FALSE)
   
   #Generate bed file
-  file.copy(from = haplo$genotypes, to = bed)
+  file.copy(from = object$genotypes, to = bed)
   
 }
