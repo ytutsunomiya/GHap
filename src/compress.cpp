9eb254ea1d5bd2c635e1914b2995de5b41ebc143
ytutsunomiya <ytutsunomiya@gmail.com>
2022-08-31 10:51:14 -0300

    included error message

--- src/compress.cpp
+++ src/compress.cpp
@@ -3,15 +3,23 @@
 #include <iostream>
 #include <fstream>
 
+// Function: compress
+// License: GPLv3 or later
+// Modification date: 31 Aug 2022
+// Written by: Yuri Tani Utsunomiya, Adam Taiti Harth Utsunomiya
+// Contact: ytutsunomiya@gmail.com, adamtaiti@gmail.com
+// Description: Auxiliary function to compress phased data into GHap binary
+
 using namespace Rcpp;
 using namespace std;
 
 // [[Rcpp::export]]
-NumericVector compress(const char* infile,
-                       const char* outfile,
-                       const int nbits,
-                       const int tbits,
-                       const int mode){
+int compress(const char* infile,
+             const char* outfile,
+             const int nunits,
+             const int nbits,
+             const int tbits,
+             const int fmode){
   
   // Expected number of characters
   int echar = 2*nbits;
@@ -28,13 +36,17 @@ NumericVector compress(const char* infile,
   // Open connection
   FILE* infilecon = fopen(infile, "r");
   ofstream outfilecon(outfile, ios::out | ios::binary);
+  int exitcode = 0;
   
   // Print magic number
-  if(mode == 1){
+  string unit = "markers";
+  if(fmode == 1){
+    unit = "markers";
     string magic = "00000001";
     bitset<8> bits(magic);
     outfilecon.write((const char*)&bits, 1);
-  }else if(mode == 2){
+  }else if(fmode == 2){
+    unit = "individuals";
     string magic = "00000010";
     bitset<8> bits(magic);
     outfilecon.write((const char*)&bits, 1);
@@ -56,19 +68,24 @@ NumericVector compress(const char* infile,
         outfilecon.write((const char*)&bits, 1);
       }
     }else{
-      fclose(infilecon);
-      outfilecon.close();
-      cerr << "[ERROR] Expected " << echar << " characters in line " << nline << " but found" << nchar;
-      return -1;
+      cerr << "\n[ERROR] Expected " << echar << " characters in line " << nline << " but found " << nchar << endl;
+      exitcode = 1;
+      goto exit;
     }
     if(nline % 1000 == 0){
-      cout << nline << " lines processed\r";
+      cout << nline << " " << unit << " written to file\r";
+    }
+    if(nline > nunits){
+      cerr << "\n[ERROR] Expected " << nunits << " lines in the phase file but found more!" << endl;
+      exitcode = 1;
+      goto exit;
     }
   }
   
   // Close files and exit
-  fclose(infilecon);
-  outfilecon.close();
-  return 0;
+  exit:
+    fclose(infilecon);
+    outfilecon.close();
+    return exitcode;
   
 }
