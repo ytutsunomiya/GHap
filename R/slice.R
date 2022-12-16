0bf0a609449132b9fc52570f131a6224abdc9bf9
ytutsunomiya <ytutsunomiya@gmail.com>
2022-12-16 17:44:22 -0300

    C++ speed up

--- R/slice.R
+++ R/slice.R
@@ -1,8 +1,8 @@
 #Function: ghap.slice
 #License: GPLv3 or later
-#Modification date: 29 Nov 2022
-#Written by: Yuri Tani Utsunomiya
-#Contact: ytutsunomiya@gmail.com
+#Modification date: 16 Dec 2022
+#Written by: Yuri Tani Utsunomiya, Adam Taiti Harth Utsunomiya
+#Contact: ytutsunomiya@gmail.com, adamtaiti@gmail.com
 #Description: Get a slice of a GHap object
 
 ghap.slice <- function(
@@ -13,353 +13,147 @@ ghap.slice <- function(
     transposed=FALSE,
     sparse=TRUE,
     unphase=FALSE,
-    impute=FALSE,
-    ncores=1,
-    verbose=TRUE
+    impute=FALSE
 ){
   
-  # Check if object is a GHap.phase object -------------------------------------
+  # Check object type ----------------------------------------------------------
   obtypes <- c("GHap.phase","GHap.haplo","GHap.plink")
   if(inherits(object, obtypes) == FALSE){
     stop("\nInput data must be a valid GHap object (phase, haplo or plink).")
   }
   
-  # Calculate offset and bitloss -----------------------------------------------
-  offset <- rep(NA, times = 2)
-  offset[1] <- ceiling((2*object$nsamples)/8)
-  offset[2] <- ceiling((2*object$nmarkers)/8)
-  bitloss <- rep(NA, times = 2)
-  bitloss[1] <- 8 - ((2*object$nsamples) %% 8)
-  if(bitloss[1] == 8){
-    bitloss[1] <- 0
-  }
-  bitloss[2] <- 8 - ((2*object$nmarkers) %% 8)
-  if(bitloss[2] == 8){
-    bitloss[2] <- 0
-  }
-  lookup <- vector(mode = "list", length = 2)
-  lookup[[1]] <- rep(NA, times=offset[1]*8)
-  lookup[[2]] <- rep(NA, times=offset[2]*8)
-  for(i in 1:offset[1]){
-    idx1 <- i*8
-    idx2 <- idx1-7
-    lookup[[1]][idx1:idx2] <- idx2:idx1
-  }
-  for(i in 1:offset[2]){
-    idx1 <- i*8
-    idx2 <- idx1-7
-    lookup[[2]][idx1:idx2] <- idx2:idx1
-  }
-  
-  # Get indices ----------------------------------------------------------------
-  if(index == TRUE){
-    
-    # Retrieve indices
-    iidx <- ids
-    vidx <- variants
-    
-    # Organize indices for ids
-    if(inherits(object, "GHap.phase")){
-      if(max(iidx) > 2*object$nsamples){
-        stop("Some of the provided ids are out of range")
-      }
-      if(object$mode == 2){
-        idstmp <- object$id[ids]
-        idstmp <- idstmp[duplicated(idstmp) == FALSE]
-        iidx <- which(object$id %in% idstmp)
-        iidx1 <- iidx[1:length(iidx) %% 2 == 1]
-        iidx2 <- iidx[1:length(iidx) %% 2 == 0]
-        names(iidx1) <- object$id[iidx1]
-        names(iidx2) <- object$id[iidx2]
-        names(ids) <- object$id[ids]
-      }
-    }else{
-      if(max(iidx) > object$nsamples){
-        stop("Some of the provided ids are out of range")
-      }
-    }
-    names(iidx) <- object$id[iidx]
-    
-    # Organize indices for variants
-    if(inherits(object, "GHap.haplo")){
-      if(max(vidx) > object$nalleles){
-        stop("Some of the provided alleles are out of range")
-      }else{
-        names(vidx) <- paste(object$block[vidx],object$bp1[vidx],
-                             object$bp2[vidx],object$allele[vidx], sep="_")
-      }
-    }else{
-      if(max(vidx) > object$nmarkers){
-        stop("Some of the provided markers are out of range")
-      }else{
-        names(vidx) <- object$marker[vidx]
-      }
-    }
-    
-    
-  }else{
-    
-    # Organize indices for ids
-    iidx <- which(object$id %in% ids)
-    if(inherits(object, "GHap.phase")){
-      if(length(iidx) != 2*length(ids)){
-        stop("Some of the provided ids were not found")
-      }else{
-        iidx1 <- iidx[1:length(iidx) %% 2 == 1]
-        iidx2 <- iidx[1:length(iidx) %% 2 == 0]
-        names(iidx1) <- object$id[iidx1]
-        names(iidx2) <- object$id[iidx2]
-        iidx1 <- iidx1[ids]
-        iidx2 <- iidx2[ids]
-        iidx <- rep(NA, times = length(iidx))
-        iidx[1:length(iidx) %% 2 == 1] <- iidx1
-        iidx[1:length(iidx) %% 2 == 0] <- iidx2
-        names(iidx)[1:length(iidx) %% 2 == 1] <- names(iidx1)
-        names(iidx)[1:length(iidx) %% 2 == 0] <- names(iidx2)
-      }
-    }else{
-      if(length(iidx) != length(ids)){
-        stop("Some of the provided ids were not found")
-      }else{
-        names(iidx) <- object$id[iidx]
-        iidx <- iidx[ids]
-      }
-    }
-    
-    # Organize indices for variants
-    if(inherits(object, "GHap.haplo")){
-      vidx <- which(1:object$nalleles %in% variants)
-      if(length(vidx) != length(variants)){
-        stop("Some of the provided marker names were not found")
-      }else{
-        vidx <- variants
-        names(vidx) <- paste(object$block[vidx],object$bp1[vidx],
-                             object$bp2[vidx],object$allele[vidx], sep="_")
-      }
-    }else{
-      vidx <- which(object$marker %in% variants)
-      if(length(vidx) != length(variants)){
-        stop("Some of the provided marker names were not found")
-      }else{
-        names(vidx) <- object$marker[vidx]
-        vidx <- vidx[variants]
-      }
-    }
-  }
-  
-  # Define bit function --------------------------------------------------------
+  # Build indices --------------------------------------------------------------
+  # iidx = individual index (1 per individual)
+  # hidx = haplotype index (2 per individual, only for mode 0 and 1)
+  # oidx = output haplotype indices (1 or 2 per individual, only for index = T)
+  # vidx = variant index
   if(inherits(object, "GHap.phase")){
-    getBitFun <- function(i){
-      if(object$mode %in% c(0,1)){
-        object.con <- file(object$phase, "rb")
-        a <- seek(con = object.con, where = object$mode + offset[1]*(vidx[i]-1),
-                  origin = 'start', rw = 'r')
-        geno <- readBin(object.con, what=raw(), size = 1,
-                        n = offset[1], signed = FALSE)
-        geno <- as.integer(rawToBits(geno))
-        geno <- geno[lookup[[1]]]
-        geno <- geno[1:(2*object$nsamples)]
-        geno <- geno[iidx]
-        close.connection(object.con)
-      }else if(object$mode == 2){
-        object.con <- file(object$phase, "rb")
-        a <- seek(con = object.con, where = 1 + offset[2]*((iidx2[i]/2)-1),
-                  origin = 'start',rw = 'r')
-        geno <- readBin(object.con, what=raw(), size = 1,
-                        n = offset[2], signed = FALSE)
-        geno <- as.integer(rawToBits(geno))
-        geno <- geno[lookup[[2]]]
-        geno <- geno[1:(2*object$nmarkers)]
-        geno1 <- geno[1:length(geno) %% 2 == 1]
-        geno2 <- geno[1:length(geno) %% 2 == 0]
-        geno1 <- geno1[vidx]
-        geno2 <- geno2[vidx]
-        if(index == TRUE){
-          iidxtmp <- ids[which(names(ids) == names(iidx2)[i])]
-          if(length(iidxtmp) == 2){
-            geno <- c(geno1,geno2)
-          }else{
-            if(iidxtmp %% 2 == 1){
-              geno <- geno1
-            }else{
-              geno <- geno2
-            }
-          }
-        }else{
-          geno <- c(geno1,geno2)
-        }
-        close.connection(object.con)
-      }
-      return(geno)
+    binfile <- object$phase
+    mode <- object$mode
+    nvars <- object$nmarkers
+    nids <- object$nsamples
+    phased <- 2
+    imp <- 0
+    if(index == TRUE){
+      idlab <- object$id[ids]
+      iidx <- 1:object$nsamples
+      names(iidx) <- object$id[1:length(object$id) %% 2 == 0]
+      iidx <- iidx[idlab[duplicated(idlab) == FALSE]]
+      x <- rep((iidx*2)-1, each=2)
+      x[1:length(x) %% 2 == 0] <- x[1:length(x) %% 2 == 0] + 1
+      y <- 1:length(x)
+      names(y) <- x
+      oidx <- y[as.character(ids)]
+      hidx <- rep(2*iidx, each=2)
+      hidx[1:length(x) %% 2 == 1] <- hidx[1:length(x) %% 2 == 1] - 1
+      vidx <- variants
+      names(vidx) <- object$marker[vidx]
+    }else{
+      iidx <- 1:object$nsamples
+      names(iidx) <- object$id[1:length(object$id) %% 2 == 0]
+      iidx <- iidx[ids]
+      hidx <- rep(2*iidx, each=2)
+      hidx[1:length(hidx) %% 2 == 1] <- hidx[1:length(hidx) %% 2 == 0]-1
+      vidx <- 1:object$nmarkers
+      names(vidx) <- object$marker
+      vidx <- vidx[variants]
     }
-  }else if(inherits(object, "GHap.haplo")){
-    getBitFun <- function(i){
-      object.con <- file(object$genotypes, "rb")
-      a <- seek(con = object.con, where = 3 + offset[1]*(vidx[i]-1),
-                origin = 'start',rw = 'r')
-      geno <- readBin(object.con, what=raw(), size = 1,
-                      n = offset[1], signed = FALSE)
-      geno <- as.integer(rawToBits(geno))
-      geno1 <- geno[1:length(geno) %% 2 == 1]
-      geno2 <- geno[1:length(geno) %% 2 == 0]
-      geno <- vector(mode = "integer", length = length(geno)/2)
-      geno[which(geno1 == 0 & geno2 == 0)] <- 0
-      geno[which(geno1 == 0 & geno2 == 1)] <- 1
-      geno[which(geno1 == 1 & geno2 == 0)] <- 1
-      geno[which(geno1 == 1 & geno2 == 1)] <- 2
-      geno <- geno[1:object$nsamples]
-      close.connection(object.con)
-      return(geno[iidx])
+    nrows <- length(vidx)
+    ncols <- 2*length(iidx)
+    rowlab <- names(vidx)
+    collab <- rep(names(iidx), each=2)
+    brow <- mode != 2
+  }
+  if(inherits(object, "GHap.plink")){
+    binfile <- object$plink
+    mode <- 3
+    nvars <- object$nmarkers
+    nids <- object$nsamples
+    phased <- 1
+    imp <- as.integer(impute)
+    if(index == TRUE){
+      iidx <- ids
+      names(iidx) <- object$id[ids]
+      hidx <- iidx
+      vidx <- variants
+      names(vidx) <- object$marker[vidx]
+    }else{
+      iidx <- 1:object$nsamples
+      names(iidx) <- object$id
+      iidx <- iidx[ids]
+      hidx <- iidx
+      vidx <- 1:object$nmarkers
+      names(vidx) <- object$marker
+      vidx <- vidx[variants]
     }
-  }else if(inherits(object, "GHap.plink")){
-    getBitFun <- function(i){
-      object.con <- file(object$plink, "rb")
-      a <- seek(con = object.con, where = 3 + offset[1]*(vidx[i]-1),
-                origin = 'start',rw = 'r')
-      geno <- readBin(object.con, what=raw(), size = 1,
-                      n = offset[1], signed = FALSE)
-      geno <- as.integer(rawToBits(geno))
-      geno1 <- geno[1:length(geno) %% 2 == 1]
-      geno2 <- geno[1:length(geno) %% 2 == 0]
-      geno <- vector(mode = "integer", length = length(geno)/2)
-      geno[which(geno1 == 0 & geno2 == 0)] <- 2
-      geno[which(geno1 == 0 & geno2 == 1)] <- 1
-      geno[which(geno1 == 1 & geno2 == 1)] <- 0
-      if(impute == TRUE){
-        geno[which(geno1 == 1 & geno2 == 0)] <- 0
-      }else{
-        geno[which(geno1 == 1 & geno2 == 0)] <- NA
-      }
-      geno <- geno[1:object$nsamples]
-      close.connection(object.con)
-      return(geno[iidx])
+    nrows <- length(vidx)
+    ncols <- length(iidx)
+    rowlab <- names(vidx)
+    collab <- names(iidx)
+    brow <- TRUE
+  }
+  if(inherits(object, "GHap.haplo")){
+    binfile <- object$genotypes
+    mode <- 3
+    nvars <- object$nalleles
+    nids <- object$nsamples
+    phased <- 1
+    imp <- 0
+    if(index == TRUE){
+      iidx <- ids
+      names(iidx) <- object$id[ids]
+      hidx <- iidx
+      vidx <- variants
+      names(vidx) <- object$allele[vidx]
+    }else{
+      iidx <- 1:object$nsamples
+      names(iidx) <- object$id
+      iidx <- iidx[ids]
+      hidx <- iidx
+      vidx <- variants
+      names(vidx) <- paste(object$block[vidx],object$bp1[vidx],
+                           object$bp2[vidx],object$allele[vidx], sep="_")
     }
+    nrows <- length(vidx)
+    ncols <- length(iidx)
+    rowlab <- names(vidx)
+    collab <- names(iidx)
+    brow <- TRUE
   }
   
-  # Get data -------------------------------------------------------------------
-  ncores <- min(c(detectCores(), ncores))
-  if(inherits(object, "GHap.phase")){
-    if(object$mode %in% c(0,1)){
-      if(ncores == 1){
-        X <- unlist(lapply(X = 1:length(vidx), FUN = getBitFun))
-      }else{
-        if(Sys.info()["sysname"] == "Windows"){
-          cl <- makeCluster(ncores)
-          clusterExport(cl = cl, varlist = list("object"), envir=environment())
-          X <- unlist(parLapply(cl = cl, fun = getBitFun, X = 1:length(vidx)))
-          stopCluster(cl)
-        }else{
-          X <- unlist(mclapply(X = 1:length(vidx), FUN = getBitFun, mc.cores = ncores))
-        }
-      }
-    }else{
-      if(ncores == 1){
-        X <- unlist(lapply(X = 1:length(iidx2), FUN = getBitFun))
-      }else{
-        if(Sys.info()["sysname"] == "Windows"){
-          cl <- makeCluster(ncores)
-          clusterExport(cl = cl, varlist = list("object"), envir=environment())
-          X <- unlist(parLapply(cl = cl, fun = getBitFun, X = 1:length(iidx2)))
-          stopCluster(cl)
-        }else{
-          X <- unlist(mclapply(X = 1:length(iidx2), FUN = getBitFun, mc.cores = ncores))
-        }
-      }
+  # Get slice of data ----------------------------------------------------------
+  X <- .sliceCpp(binfile, mode, nvars, nids, phased, imp, iidx, hidx, vidx)
+
+  # Organize output matrix -----------------------------------------------------
+  if(transposed == FALSE){
+    X <- Matrix(data = X, nrow = nrows, ncol = ncols,
+                byrow = brow, sparse = TRUE)
+    rownames(X) <- rowlab
+    colnames(X) <- collab
+    if(unphase == TRUE & phased == 2){
+      cols <- 1:ncol(X) %% 2
+      X <- X[,which(cols == 1)] + X[,which(cols == 0)]
+    }else if(index == TRUE & phased == 2){
+      X <- X[,oidx]
     }
   }else{
-    if(ncores == 1){
-      X <- unlist(lapply(X = 1:length(vidx), FUN = getBitFun))
-    }else{
-      if(Sys.info()["sysname"] == "Windows"){
-        cl <- makeCluster(ncores)
-        clusterExport(cl = cl, varlist = list("object"), envir=environment())
-        X <- unlist(parLapply(cl = cl, fun = getBitFun, X = 1:length(vidx)))
-        stopCluster(cl)
-      }else{
-        X <- unlist(mclapply(X = 1:length(vidx), FUN = getBitFun, mc.cores = ncores))
-      }
+    X <- Matrix(data = X, nrow = ncols, ncol = nrows, 
+                byrow = (!brow), sparse = TRUE)
+    rownames(X) <- collab
+    colnames(X) <- rowlab
+    if(unphase == TRUE & phased == 2){
+      rows <- 1:nrow(X) %% 2
+      X <- X[which(rows == 1),] + X[which(rows == 0),]
+    }else if(index == TRUE & phased == 2){
+      X <- X[oidx,]
     }
   }
   
-  if(inherits(object, "GHap.phase")){
-    if(object$mode %in% c(0,1)){
-      if(transposed == FALSE){
-        X <- Matrix(data = X, nrow = length(vidx), ncol = length(iidx),
-                    byrow = TRUE, sparse = TRUE)
-        colnames(X) <- names(iidx)
-        rownames(X) <- names(vidx)
-        if(unphase == TRUE){
-          cols <- 1:ncol(X) %% 2
-          X <- X[,which(cols == 1)] + X[,which(cols == 0)]
-        }
-      }else{
-        X <- Matrix(data = X, ncol = length(vidx), nrow = length(iidx),
-                    byrow = FALSE, sparse = TRUE)
-        rownames(X) <- names(iidx)
-        colnames(X) <- names(vidx)
-        if(unphase == TRUE){
-          rows <- 1:nrow(X) %% 2
-          X <- X[which(rows == 1),] + X[which(rows == 0),]
-        }
-      }
-    }else if(object$mode == 2){
-      if(transposed == FALSE){
-        if(index == TRUE){
-          X <- Matrix(data = X, nrow = length(vidx), ncol = length(ids),
-                      byrow = FALSE, sparse = TRUE)
-          colnames(X) <- names(sort(rank(ids)))
-          rownames(X) <- names(vidx)
-          X <- X[,rank(ids)]
-        }else{
-          X <- Matrix(data = X, nrow = length(vidx), ncol = 2*length(iidx2),
-                      byrow = FALSE, sparse = TRUE)
-          colnames(X) <- rep(names(iidx2), each = 2)
-          rownames(X) <- names(vidx)
-        }
-        if(unphase == TRUE){
-          cols <- 1:ncol(X) %% 2
-          X <- X[,which(cols == 1)] + X[,which(cols == 0)]
-        }
-      }else{
-        if(index == TRUE){
-          X <- Matrix(data = X, ncol = length(vidx), nrow = length(ids),
-                      byrow = TRUE, sparse = TRUE)
-          rownames(X) <- names(sort(rank(ids)))
-          colnames(X) <- names(vidx)
-          X <- X[,rank(ids)]
-        }else{
-          X <- Matrix(data = X, ncol = length(vidx), nrow = 2*length(iidx2),
-                      byrow = TRUE, sparse = TRUE)
-          rownames(X) <- rep(names(iidx2), each = 2)
-          colnames(X) <- names(vidx)
-          X <- X[,rank(ids)]
-        }
-        if(unphase == TRUE){
-          rows <- 1:nrow(X) %% 2
-          X <- X[which(rows == 1),] + X[which(rows == 0),]
-        }
-      }
-    }
-  }else{
-    if(transposed == FALSE){
-      X <- Matrix(data = X, nrow = length(vidx), ncol = length(iidx),
-                  byrow = TRUE, sparse = TRUE)
-      colnames(X) <- names(iidx)
-      rownames(X) <- names(vidx)
-    }else{
-      X <- Matrix(data = X, ncol = length(vidx), nrow = length(iidx),
-                  byrow = FALSE, sparse = TRUE)
-      rownames(X) <- names(iidx)
-      colnames(X) <- names(vidx)
-    }
-  }
-  
-  
+  # Check if output should be sparse -------------------------------------------
   if(sparse == FALSE){
     X <- as.matrix(X)
   }
-  
-  
+
   # Return X -------------------------------------------------------------------
   return(X)
   
