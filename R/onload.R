31e8b493b3cc88f24e41430291bf9c49ab60cb89
ytutsunomiya <ytutsunomiya@gmail.com>
2025-07-17 16:08:40 -0300

    Update onload.R

--- R/onload.R
+++ R/onload.R
@@ -1,9 +1,9 @@
 .onAttach <- function(libname, pkgname) {
   mymsg <- "Loading required package: GHap\n\n\n"
-  mymsg <- paste(mymsg,"Thanks for using GHap v4.0.0.12 (beta/dev)!\n")
+  mymsg <- paste(mymsg,"Thanks for using GHap v4.0.0.13 (beta/dev)!\n")
   mymsg <- paste(mymsg,"For more information use: help(package = 'GHap')\n")
   mymsg <- paste(mymsg,"                          citation(package = 'GHap')\n")
   mymsg <- paste(mymsg,"                          browseVignettes(package = 'GHap')\n\n")
-  mymsg <- paste(mymsg,"Version date: 11 Oct 2024\n\n")
+  mymsg <- paste(mymsg,"Version date: 17 Jul 2025\n\n")
   packageStartupMessage(mymsg)
 }
