ffc806142345db547a6585c45800e2a591908b74
ytutsunomiya <ytutsunomiya@gmail.com>
2025-07-17 15:02:10 -0300

    Update kinship.R
    Included an ancestry-weighted matrix

--- R/kinship.R
+++ R/kinship.R
@@ -1,6 +1,6 @@
 #Function: ghap.kinship
 #License: GPLv3 or later
-#Modification date: 19 May 2023
+#Modification date: 17 Jul 2025
 #Written by: Yuri Tani Utsunomiya
 #Contact: ytutsunomiya@gmail.com
 #Description: Compute relationship matrix
@@ -9,11 +9,13 @@ ghap.kinship <- function(
     object,
     weights=NULL,
     sparsity=NULL,
-    type=1,
+    type=3,
     batchsize=NULL,
     freq=NULL,
+    ancestry=NULL,
     idrow=NULL,
     idcol=NULL,
+    outputM = FALSE,
     only.active.samples=TRUE,
     only.active.variants=TRUE,
     ncores=1,
@@ -72,17 +74,31 @@ ghap.kinship <- function(
     var.in <- which(object$marker.in)
   }
   
-  # Check frequency vector -----------------------------------------------------
+  # Check frequency argument ---------------------------------------------------
   if(is.null(freq) == FALSE){
-    freq <- freq[which(names(freq) %in% object$marker[var.in])]
-    if(length(freq) != length(var.in)){
-      stop("\nFrequency vector must include all active markers.")
+    if(type == 7){
+      if(is.data.frame(freq) == FALSE | is.data.frame(ancestry) == FALSE){
+        stop("\ntype = 7 requires data frames for arguments 'freq' and 'ancestry'.")
+      }else if(identical(colnames(freq),colnames(ancestry)) == FALSE){
+        stop("\ntype = 7 requires columns in 'freq' and 'ancestry' to be identical.")
+      }else if(ncol(freq) <= 2){
+        stop("\ntype = 7 requires at least two columns in data frames 'freq' and 'ancestry'.")
+      }
     }else{
-      freq <- freq[object$marker[var.in]]
+      if(is.vector(freq) == FALSE){
+        stop("\nArgument 'freq' does not seem to be a named vector.")
+      }else{
+        freq <- freq[which(names(freq) %in% object$marker[var.in])]
+        if(length(freq) != length(var.in)){
+          stop("\nFrequency vector must include all active markers.")
+        }else{
+          freq <- freq[object$marker[var.in]]
+        }
+      }
     }
   }
   
-  #Check weights
+  # Check weights --------------------------------------------------------------
   if (is.null(weights) == FALSE & length(weights) != var.n) {
     stop("Vector of weights must have the same length as the number of variants.")
   }
@@ -104,21 +120,21 @@ ghap.kinship <- function(
     id1 <- id1[-length(id1)]; id2 <- id2[-length(id2)]
   }
   
-  #Log message -----------------------------------------------------------------
+  # Log message ----------------------------------------------------------------
   if(verbose == TRUE){
     cat("Processing ", var.n, " variants in ", length(id1), " batches.\n", sep="")
     cat("Inactive variants will be ignored.\n")
   }
   
-  #Initialize allele frequency function ----------------------------------------
+  # Initialize allele frequency function ---------------------------------------
   freqfun <- function(x){
     n <- length(which(is.na(x) == FALSE))
     p <- sum(x, na.rm = TRUE)/(2*n)
     return(p)
   }
   
-  #Initialize scaling function -------------------------------------------------
-  scalefun <- vector(mode = "list", length = 6)
+  # Initialize scaling function ------------------------------------------------
+  scalefun <- vector(mode = "list", length = 7)
   scalefun[[1]] <- function(x){
     m <- mean(x, na.rm = TRUE)
     aa <- which(x == 0)
@@ -177,17 +193,26 @@ ghap.kinship <- function(
     return(x)
   }
   scalefun[[6]] <- scalefun[[5]]
+  scalefun[[7]] <- function(j){
+    a <- as.numeric(ancestry[j,])
+    plk <- rowSums(t(t(subfreq)*a))
+    m <- 2*plk
+    s <- sqrt(m*(1-plk))
+    x <- (Mtmp[,j] - m)/s
+    return(x)
+  }
   
-  #Initialize denominators -----------------------------------------------------
-  scaleval <- vector(mode = "list", length = 6)
+  # Initialize denominators ----------------------------------------------------
+  scaleval <- vector(mode = "list", length = 7)
   scaleval[[1]] <- function(){return(mean(diag(K)))}
   scaleval[[2]] <- function(){return(var.n)}
   scaleval[[3]] <- function(){return(2*sum(p*(1-p)))}
   scaleval[[4]] <- scaleval[[2]]
   scaleval[[5]] <- function(){return(4*sum(p^2*(1-p)^2))}
   scaleval[[6]] <- scaleval[[1]]
+  scaleval[[7]] <- scaleval[[2]]
   
-  #Initialize kinship matrix ---------------------------------------------------
+  # Initialize kinship matrix --------------------------------------------------
   if(verbose == TRUE){
     if(is.null(idrow) & is.null(idcol)){
       cat("Preparing", id.n, "x", id.n, "kinship matrix.\n")
@@ -201,52 +226,64 @@ ghap.kinship <- function(
     K <- Matrix(data = 0, nrow = length(idrow), ncol = length(idcol), doDiag = F)
   }
   
-  #Kinship iterate function ----------------------------------------------------
+  # Kinship iterate function ---------------------------------------------------
   ncores <- min(c(detectCores(), ncores))
   sumvariants <- 0
   q <- 0
+  M <- NULL
   for(i in 1:length(id1)){
     idx <- id1[i]:id2[i]
-    Ztmp <- ghap.slice(object = object,
+    Mtmp <- ghap.slice(object = object,
                        ids = id.in,
                        variants = var.in[idx],
                        index = TRUE,
                        unphase = TRUE,
                        impute = TRUE)
-    zids <- colnames(Ztmp)
-    if(is.null(freq)){
-      p <- apply(X = Ztmp, MARGIN = 1, FUN = freqfun)
+    mids <- colnames(Mtmp)
+    if(type != 7){
+      if(is.null(freq)){
+        p <- apply(X = Mtmp, MARGIN = 1, FUN = freqfun)
+      }else{
+        p <- freq[object$marker[var.in[idx]]]
+      }
+      exc <- which(pmin(p,1-p) == 0)
+      if(length(exc) > 0){
+        p <- p[-exc]
+        Mtmp <- Mtmp[-exc,]
+        var.n <- var.n - length(exc)
+      }
+      if(type %in% c(3,5)){
+        q <- q + scaleval[[type]]() 
+      }
+      if(type == 3 & is.null(freq) == FALSE){
+        Mtmp <- t(Mtmp - 2*p)
+      }else{
+        Mtmp <- apply(X = Mtmp, MARGIN = 1, FUN = scalefun[[type]])
+      }
     }else{
-      p <- freq[object$marker[var.in[idx]]]
+      subfreq <- freq[rownames(Mtmp),]
+      out <- unlist(mclapply(X = 1:ncol(Mtmp), FUN = scalefun[[7]], mc.cores = ncores))
+      Mtmp <- Matrix(data = out, ncol = nrow(Mtmp), nrow = ncol(Mtmp), byrow = TRUE)
+      rownames(Mtmp) <- mids
+      colnames(Mtmp) <- object$marker[var.in[idx]]
     }
-    exc <- which(pmin(p,1-p) == 0)
-    if(length(exc) > 0){
-      p <- p[-exc]
-      Ztmp <- Ztmp[-exc,]
-      var.n <- var.n - length(exc)
-    }
-    if(type %in% c(3,5)){
-      q <- q + scaleval[[type]]() 
-    }
-    if(type == 3 & is.null(freq) == FALSE){
-      Ztmp <- t(Ztmp - 2*p)
-    }else{
-      Ztmp <- apply(X = Ztmp, MARGIN = 1, FUN = scalefun[[type]])
+    if(outputM == TRUE){
+      M <- rbind(M, t(Mtmp))
     }
     if(is.null(weights) == FALSE){
-      Ztmp*sqrt(weights[idx])
+      Mtmp*sqrt(weights[idx])
     }
     if(is.null(idrow) & is.null(idcol)){
-      K <- K + tcrossprod(Ztmp)
+      K <- K + tcrossprod(Mtmp)
     }else{
       if(nrow(K) == 1 & ncol(K) > 1){
-        K <- K + t(Ztmp[idcol,]%*%Ztmp[idrow,])
+        K <- K + t(Mtmp[idcol,]%*%Mtmp[idrow,])
       }else if(nrow(K) > 1 & ncol(K) == 1){
-        K <- K + Ztmp[idrow,]%*%Ztmp[idcol,]
+        K <- K + Mtmp[idrow,]%*%Mtmp[idcol,]
       }else if(nrow(K) == 1 & ncol(K) == 1){
-        K <- K + sum(Ztmp[idrow,]*Ztmp[idcol,])
+        K <- K + sum(Mtmp[idrow,]*Mtmp[idcol,])
       }else{
-        K <- K + tcrossprod(Ztmp[idrow,],Ztmp[idcol,])
+        K <- K + tcrossprod(Mtmp[idrow,],Mtmp[idcol,])
       }
     }
     if(verbose == TRUE){
@@ -255,25 +292,33 @@ ghap.kinship <- function(
     }
   }
   
-  #Scale kinship matrix -------------------------------------------------------
+  # Scale kinship matrix -------------------------------------------------------
   if(type %in% c(3,5) == FALSE){
     q <- scaleval[[type]]() 
   }
   K <- K/q
+  
+  # Induce sparsity ------------------------------------------------------------
+  if(is.null(sparsity) == FALSE){
+    K <- drop0(K, tol = sparsity)
+  }else{
+    K <- as.matrix(K)
+  }
+  
+  # Assign row and column names ------------------------------------------------
   if(is.null(idrow) & is.null(idcol)){
-    colnames(K) <- zids
+    colnames(K) <- mids
     rownames(K) <- colnames(K)
   }else{
     rownames(K) <- idrow
     colnames(K) <- idcol
   }
   
-  #Induce sparsity ------------------------------------------------------------
-  if(is.null(sparsity) == FALSE){
-    K <- drop0(K, tol = sparsity)
+  # Return output --------------------------------------------------------------
+  if(outputM == TRUE){
+    return(list(K = K, M = M))
+  }else{
+    return(K)
   }
-  
-  #Return output --------------------------------------------------------------
-  return(K)
   
 }
